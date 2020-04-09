f6dd6e6baee26ce8af2378412d1a686e9efa4b35
cosney <nick@Nicks-iMac.local>
2020-04-09 16:13:03 -0700

    REworkr of the Recursion

--- checkers/srcs/black_spot.c
+++ checkers/srcs/black_spot.c
@@ -6,7 +6,7 @@
 /*   By: cosney <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/05 17:53:04 by cosney            #+#    #+#             */
-/*   Updated: 2020/04/09 14:17:33 by cosney           ###   ########.fr       */
+/*   Updated: 2020/04/09 16:06:17 by cosney           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,24 +15,25 @@
 #include <string.h>
 #define pos_size 2
 
-struct array
+void	new_str_tmp(char *str_tmp)
 {
-	char v[100];
-	char w[100];
-} ar;
-
-int 	two_choses(char bd[][10], int x, int y)
-{
-	if ((bd[x + 1][y - 1] == 'b' && bd[x + 2][y - 2] == '.') 
-		&& (bd[x + 1][y + 1] == 'b'	&& bd[x + 2][y + 2] == '.'))
-		return (1);
-	return (0);
+	int		index;
+	
+	index = 0;
+	while (str_tmp[index] != '\0')
+	{
+		index++;
+	}
+	str_tmp[index - 1] = '\0';
+	str_tmp[index - 2] = '\0';
 }
 
 int		mv(char bd[][10], int x, int y, char *st, char *str_tmp)
 {
 	char 	a[pos_size];
 	char 	b[pos_size];
+	int 	index;
+	int		z;
 
 	zeros(a, pos_size);
 	zeros(b, pos_size);
@@ -40,17 +41,19 @@ int		mv(char bd[][10], int x, int y, char *st, char *str_tmp)
 	b[0] = y + '0';
 	strcat(str_tmp, a);
 	strcat(str_tmp, b);
-	if (two_choses(bd, x, y))
-	{
-		strcpy(ar.v, str_tmp);
-		strcpy(ar.w, str_tmp);
-		if (mv(bd, x + 2, y + 2, st, ar.v) && mv(bd, x + 2, y - 2, st, ar.w))
-			return (0);
-	}
 	if (bd[x + 1][y - 1] == 'b'	&& bd[x + 2][y - 2] == '.')
-		return(mv(bd, x + 2, y - 2, st, str_tmp));
+	{
+		bd[x + 1][y - 1] = '#';
+		mv(bd, x + 2, y - 2, st, str_tmp);
+		bd[x + 1][y - 1] = 'b';
+	}
 	if (bd[x + 1][y + 1] == 'b'	&& bd[x + 2][y + 2] == '.')	
-		return(mv(bd, x + 2, y + 2, st,  str_tmp));
+	{
+		bd[x + 1][y + 1] = '#';
+		mv(bd, x + 2, y + 2, st,  str_tmp);
+		bd[x + 1][y + 1] = 'b';
+	}
 	strcat(st, str_tmp);
+	new_str_tmp(str_tmp);
 	return (0);
 }
