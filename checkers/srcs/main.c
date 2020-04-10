c3b33b373c70b9202f6d70f78fc955cb7d14f0dd
cosney <nick@Nicks-iMac.local>
2020-04-09 17:07:29 -0700

    IT WORKS

--- checkers/srcs/main.c
+++ checkers/srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: cosney <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/05 11:52:42 by cosney            #+#    #+#             */
-/*   Updated: 2020/04/09 13:25:58 by cosney           ###   ########.fr       */
+/*   Updated: 2020/04/09 16:55:02 by cosney           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,19 +17,33 @@
 int		solution(char board[][10])
 {
 	int w_pos[2];
+	int	q_pos[2];
 	char str[a_size];
 	char str_tmp[a_size];
+	int i;
 
+	i = 0;
 	zeros(str, a_size);
 	zeros(str_tmp, a_size);
 	while (1)
 	{
 		strcat(str_tmp, ".");
 		white_spot(board, w_pos);
+		//queen_spot(board, q_pos);
 		if (w_pos[0] == -1)
 				break;
 		mv(board, w_pos[0], w_pos[1], str, str_tmp);
-		memset(str_tmp, '\0', 100);
+		//queen_mv(board, q_pos[0], q_pos[1], str, str_tmp);
+	}
+	while (1)
+	{
+		if (i > 0)
+			strcat(str_tmp, ".");
+		queen_spot(board, q_pos);
+		if (q_pos[0] == -1)
+			break;
+		queen_mv(board, q_pos[0], q_pos[1], str, str_tmp);
+		i++;
 	}
 	longest_way(str);
 	return (0);
--- checkers/srcs/queen_mv.c
+++ checkers/srcs/queen_mv.c
@@ -1,21 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   black_spot.c                                       :+:      :+:    :+:   */
+/*   queen_mv.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: cosney <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/04/05 17:53:04 by cosney            #+#    #+#             */
-/*   Updated: 2020/04/09 16:39:53 by cosney           ###   ########.fr       */
+/*   Created: 2020/04/09 16:17:31 by cosney            #+#    #+#             */
+/*   Updated: 2020/04/09 16:26:46 by cosney           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
 
 #include "header.h"
 #include <stdio.h>
 #include <string.h>
 #define pos_size 2
 
-void	new_str_tmp1(char *str_tmp)
+void	new_str_tmp(char *str_tmp)
 {
 	int		index;
 	
@@ -28,7 +29,7 @@ void	new_str_tmp1(char *str_tmp)
 	str_tmp[index - 2] = '\0';
 }
 
-int		mv(char bd[][10], int x, int y, char *st, char *str_tmp)
+int		queen_mv(char bd[][10], int x, int y, char *st, char *str_tmp)
 {
 	char 	a[pos_size];
 	char 	b[pos_size];
@@ -44,16 +45,27 @@ int		mv(char bd[][10], int x, int y, char *st, char *str_tmp)
 	if (bd[x + 1][y - 1] == 'b'	&& bd[x + 2][y - 2] == '.')
 	{
 		bd[x + 1][y - 1] = '#';
-		mv(bd, x + 2, y - 2, st, str_tmp);
+		queen_mv(bd, x + 2, y - 2, st, str_tmp);
 		bd[x + 1][y - 1] = 'b';
 	}
 	if (bd[x + 1][y + 1] == 'b'	&& bd[x + 2][y + 2] == '.')	
 	{
 		bd[x + 1][y + 1] = '#';
-		mv(bd, x + 2, y + 2, st,  str_tmp);
+		queen_mv(bd, x + 2, y + 2, st,  str_tmp);
 		bd[x + 1][y + 1] = 'b';
 	}
-	strcat(st, str_tmp);
-	new_str_tmp1(str_tmp);
+		if (bd[x - 1][y - 1] == 'b'	&& bd[x - 2][y - 2] == '.')
+	{
+		bd[x - 1][y - 1] = '#';
+		queen_mv(bd, x - 2, y - 2, st, str_tmp);
+		bd[x - 1][y - 1] = 'b';
+	}
+	if (bd[x - 1][y + 1] == 'b'	&& bd[x - 2][y + 2] == '.')	
+	{
+		bd[x - 1][y + 1] = '#';
+		queen_mv(bd, x - 2, y + 2, st,  str_tmp);
+		bd[x - 1][y + 1] = 'b';
+	}	strcat(st, str_tmp);
+	new_str_tmp(str_tmp);
 	return (0);
 }
